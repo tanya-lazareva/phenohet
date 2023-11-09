0e3f7d7327e7764a7ee0094abf351de7c50b39f4
tanya-lazareva <92986766+tanya-lazareva@users.noreply.github.com>
2023-11-09 17:40:48 +0300

    Delete script/random_simscore.sh

--- script/random_simscore.sh
+++
@@ -1,8 +0,0 @@
-#! usr/bin/bash
-repeats=530
-for i in $(seq $repeats); do python3 ./script/countsimilarity_random.py --genedis ./count_HPO/G1Ph_MIM.csv --hpo ./count_HPO/terms_to_mim.txt; done > ./count_HPO/G1Ph_similarity_r.csv
-sed 3~2d ./count_HPO/G1Ph_similarity_r.csv > ./count_HPO/G1Ph_simscore.csv
-rm ./count_HPO/G1Ph_similarity_r.csv
-
-
-
