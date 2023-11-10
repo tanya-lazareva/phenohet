02691fa8d87b2d63f327f04aefaf4dcdc78bf692
tanya-lazareva <92986766+tanya-lazareva@users.noreply.github.com>
2023-11-10 12:56:31 +0300

    Required Python and R packages

---
+++ requirements.R
@@ -0,0 +1,19 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2639
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww29200\viewh18400\viewkind0
+\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 clusterProfiler 3.14.3\
+dplyr 1.1.3\
+enrichplot 1.6.1\
+hexbin 1.28.2\
+qqman 0.1.9\
+ggplot2 3.4.3\
+gridExtra 2.3\
+msigdbr 7.5.1\
+RColorBrewer 1.1.3\
+rlang 1.1.1\
+scales 1.2.1\
+tidyr 1.3.0}
